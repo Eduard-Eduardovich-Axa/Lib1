2e88fd9019597807841cbba23434bfba326b444b
Eduard-Eduardovich-Axa <krygineduard@gmail.com>
2024-12-23 19:49:59 +0300

    Добавьте файлы проекта.

---
+++ Lib1/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+
+
+namespace Lib1
+{
+    public class CustomTextBox : TextBox
+    {
+        public CustomTextBox()
+        {
+            // Здесь можно настроить свойства по умолчанию
+            this.BorderStyle = BorderStyle.FixedSingle;
+            this.Font = new Font("Arial", 12);
+        }
+    }
+
+    public class CustomButton : Button
+    {
+        public CustomButton()
+        {
+            // Здесь можно настроить свойства по умолчанию
+            this.Text = "uажмите меня";
+            this.BackColor = Color.FromArgb(175, 238, 238);
+        }
+    }
+
+    public class CustomLabel : Label
+    {
+        public CustomLabel()
+        {
+            // Здесь можно настроить свойства по умолчанию
+            this.Font = new Font("Arial", 10);
+            this.AutoSize = true;
+            this.ForeColor = Color.DarkBlue;
+        }
+    }
+
+    public class CustomListBox : ListBox
+    {
+        public CustomListBox()
+        {
+            // Здесь можно настроить свойства по умолчанию
+            this.SelectionMode = SelectionMode.MultiExtended;
+        }
+    }
+}
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using Lib1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lib1;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            CustomTextBox textBox1 = new CustomTextBox();
+            textBox1.Location = new Point(10, 10);
+            textBox1.Text = "вводи сюда";
+            this.Controls.Add(textBox1);
+
+            CustomButton button1 = new CustomButton();
+            button1.Location = new Point(10, 50);
+            button1.Text = "сюда тыкай";
+            button1.BackColor = Color.FromArgb(175, 238, 238);
+
+            CustomTextBox textBox2 = new CustomTextBox();
+            textBox2.Location = new Point(10, 90);
+            textBox2.Text = "сюда не вводи";
+            textBox2.Width = 150;
+            textBox2.ReadOnly = true;
+            this.Controls.Add(textBox2);
+
+            this.Controls.Add(button1);
+            CustomLabel Label1 = new CustomLabel();
+            Label1.Location = new Point(10, 90);
+           // Label1.Text = "меняйзначение";
+            this.Controls.Add(Label1);
+
+            CustomListBox ListBox1 = new CustomListBox();
+            ListBox1.Location = new Point(10, 130);
+            //ListBox1.Items.Add(123);
+            this.Controls.Add(ListBox1);
+
+           /* textBox1.TextChanged += (sender, e) =>
+            {
+                button1.Text = textBox1.Text;
+            };*/
+
+            button1.Click += (sender, e) =>
+            {
+                ListBox1.Items.Add(textBox1.Text);
+            };
+
+
+
+            // Аналогично добавляем другие элементы
+        }
+    }
+}
